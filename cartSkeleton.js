e56ad1dde506cc391eab1a28bb11030735dd91a3
Olivia-RO <62313189+Olivia-RO@users.noreply.github.com>
2020-12-02 14:08:15 -0700

    Add files via upload

---
+++ cartSkeleton.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var createError = require('http-errors');
+var path = require('path');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+
+/*
+this was for an index.js file, am putting this here
+becuase its small and I didn't want to confuse for the other project on
+this git rep
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+*/ 
